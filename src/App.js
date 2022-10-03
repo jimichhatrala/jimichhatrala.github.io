a889c025833c04f001f09fe9a3d97edd58c183bf
jimichhatrala <jimichhatrala@gmail.com>
2022-10-03 18:13:16 +0530

    Project Under Construction

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p> 👷‍♂️ 🚧 Site Under Construction 🚧 👷‍♂️</p>
+        <a className="App-link" href="#" rel="noopener noreferrer">
+          🙋‍♂️ Please Visit Again Later..🏠
         </a>
       </header>
     </div>
