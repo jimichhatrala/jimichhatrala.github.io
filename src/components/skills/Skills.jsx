57227f5faf15a14c3ce64be14925d58b537ccf18
jimichhatrala <jimichhatrala@gmail.com>
2022-10-08 17:32:00 +0530

    Services Section and Modal Added

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -5,7 +5,7 @@ import "./skills.css";
 
 const Skills = () => {
   return (
-    <section className="skills section" id="skilld">
+    <section className="skills section" id="skills">
       <h2 className="section__title">Skills</h2>
       <span className="section__subtitle">My Technical Level</span>
       <div className="skills__container container grid">
