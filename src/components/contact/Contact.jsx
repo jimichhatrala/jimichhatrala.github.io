66732615d583fea99a2186ee1f6e6c255270fd8b
jimichhatrala <jimichhatrala@gmail.com>
2022-10-20 22:51:54 +0530

    Contact Section Top Gap / Header Active Link

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,7 +3,7 @@ import "./contact.css";
 
 const Contact = () => {
   return (
-    <section className="contact person" id="contact">
+    <section className="contact section" id="contact">
       <h2 className="section__title">Get in touch</h2>
       <span className="section__subtitle">Contact Me</span>
 
